8ba4a555f6b9506dde16487f7d9def31ec91daf0
juststart <lc_aslan@163..com>
2019-09-11 00:00:42 +0800

    项目初始化

---
+++ src/view/hello.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <span class="test1" @click="clicksomething">{{msg}}</span>
+    <div class="test"></div>
+  </div>
+</template>
+<<script>
+export default {
+    data(){
+      return{
+        msg:"hello,mini-vue-cli"
+      }
+      
+    },
+    methods:{
+      clicksomething(){
+        console.log(1)
+      }
+    },
+}
+</script>
+<style lang="scss">
+.test1 {
+  display:flex;
+  color: orange;
+  font-size: 30px;
+  &:hover {
+    color: yellow;
+  }
+}
+</style>
